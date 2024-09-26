9bb04e6f9959b7ebf630f67446230cc20acc696e
Nick <sns56@students.uwf.edu>
2024-09-26 17:08:06 -0500

    okay pog

---
+++ Yeah (IR Sensor).cpp
@@ -0,0 +1,27 @@
+//IR Sensor (with GPIO commands to help Raspberry Pi?)
+
+//Variable definitions
+const int LED = 2; //LED is on pin 2
+const int IR = 3; //IR Sensor is on pin 3
+
+void setup(){
+
+    Serial.begin(9600); //Opens serial port & sets data rate to 9600 bps
+
+    GPIO.pinMode(LED, GPIO.OUTPUT); //Sets LED as the output
+    GPIO.pinMode(IR, GPIO.INPUT); //Sets IR Sensor as the input
+}
+
+void loop(){
+    int thing = digitalRead(IR); //Thing is equal to the current state of the IR Sensor
+
+    if (thing == GPIO.LOW) {
+        digitalWrite(LED, GPIO.HIGH); //Turns the LED on
+    }
+
+    else {
+        digitalWrite(LED, GPIO.LOW); //Turns the LED off
+    }
+
+    delay(1000); //Delay to prevent inputs stacking up. Yippee!
+}
