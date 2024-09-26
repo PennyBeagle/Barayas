69aab4df178bc6b7797126ef81ca70a26ad8b332
Nick <sns56@students.uwf.edu>
2024-09-26 16:37:15 -0500

    LA code

--- LA.cpp
+++ LA.cpp
@@ -1,5 +1,35 @@
 //Linear Actuator
 
 void setup(){
-    pinMode()
+    pinMode(1, OUTPUT); //Pin 1 is now an output
+    pinMode(2, OUTPUT); //Pin 2 is now an output
+
+    digitalWrite(1, HIGH); //Sets pin 1 to Low
+    digitalWrite(2, HIGH); //Sets pin 2 to Low
+}
+
+void(loop){
+    //Extension - This is just an initial setup. If 1 = Low and 2 = High, the LA extends
+    digitalWrite(1, LOW);
+    digitalWrite(2, HIGH);
+
+    delay(1000); //We are indeed chilling
+
+    //Stop - If both 1 and 2 = High, the LA stops moving
+    digitalWrite(1, HIGH);
+    digitalWrite(2, HIGH);
+
+    delay(1000); //Again. Chillin'
+
+    //Retraction - If 1 = High and 2 = Low, the LA retracts
+    digitalWrite(1, HIGH);
+    digitalWrite(2, LOW);
+
+    delay(1000); //Wow we do use these delays a lot don't we
+
+    //Stop - You get the gist
+    digitalWrite(1, HIGH);
+    digitalWrite(2, HIGH);
+
+    delay(1000);
 }
