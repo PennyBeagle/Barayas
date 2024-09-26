46f7c4a267b763ae87d6f2ae6f40ebd4938c9cfe
Nick <sns56@students.uwf.edu>
2024-09-26 16:13:27 -0500

    IR Sensor finished

---
+++ IR Sensor.cpp
@@ -0,0 +1,25 @@
+//Variable definitions
+const int LED = 2; //LED is on pin 2
+const int IR = 3; //IR Sensor is on pin 3
+
+void setup(){
+
+    Serial.begin(9600); //Opens serial port & sets data rate to 9600 bps
+
+    pinMode(LED, OUTPUT); //Sets LED as the output
+    pinMode(IR, INPUT); //Sets IR Sensor as the input
+}
+
+void loop(){
+    int thing = digitalRead(IR); //Thing is equal to the current state of the IR Sensor
+
+    if (thing == LOW) {
+        digitalWrite(LED, HIGH); //Turns the LED on
+    }
+
+    else {
+        digitalWrite(LED, LOW); //Turns the LED off
+    }
+
+    delay(1000); //A delay, as a treat
+}
