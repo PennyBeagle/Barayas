a8ef112901c7cdba608f4eaa019b61311aaf4dac
Nick <sns56@students.uwf.edu>
2025-02-11 10:04:04 -0600

    this is late my bad :(

--- ARD SRV.cpp
+++ ARD SRV.cpp
@@ -1,7 +1,30 @@
 //Servo Motors :D
 
 #include <Servo.h> //Library for Servos on Arduino
-#include "ARD SRV.hpp"
+
+void RotateRight (Servo prototype, int pos){
+    //SRVO movement
+    for (pos = 0; pos <= 180; pos += 1){ //Moves 1 degree every time the loop cycles
+      prototype.write(pos); //Pos1 changes within the for loop, and this command tells the servo to move to that position
+      delay(10); //A 10 ms delay
+    }
+}
+
+void RotateLeft (Servo prototype, int pos){
+    //SRVO movement
+    for (pos = 180; pos >= 0; pos -= 1){ //Moves 1 degree every time the loop cycles
+      prototype.write(pos); //Pos1 changes within the for loop, and this command tells the servo to move to that position
+      delay(10); //A 10 ms delay
+    }
+}
+
+//Variable definitions
+Servo SRVO1; //Creates the SRVO1 servo
+Servo SRVO2; //Creates the SRVO2 servo
+Servo SRVO3; //Creates the SRVO3 servo
+Servo SRVO4; //Creates the SRVO4 servo
+
+int pos = 0; //Variable used to store the servo position
 
 void setup(){
     Serial.begin(9600);
@@ -12,59 +35,19 @@ void setup(){
 }
 
 void loop(){
-    //The first for loop makes the servo turn from 0 degrees to 180 degrees
+    //The first function call makes the servo turn from 0 degrees to 180 degrees
 
-    //SRVO 1
-    for (pos1 = 0; pos1 <= 180; pos1 += 1){ //Moves 1 degree every time the loop cycles
-      SRVO1.write(pos1); //Pos1 changes within the for loop, and this command tells the servo to move to that position
-      delay(10); //A 10 ms delay
-    }
+    RotateRight (SRVO1, pos); //Makes SRVO1 rotate
+    RotateRight (SRVO2, pos); //Makes SRVO2 rotate
+    RotateRight (SRVO3, pos); //Makes SRVO3 rotate
+    RotateRight (SRVO4, pos); //Makes SRVO4 rotate
 
-    //SRVO 2
-    for (pos2 = 0; pos2 <= 180; pos2 += 1){ //Moves 1 degree every time the loop cycles
-      SRVO2.write(pos2); //Pos2 changes within the for loop, and this command tells the servo to move to that position
-      delay(10); //A 10 ms delay
-    }
+    //The second function call moves it from 180 degrees to 0 degrees
 
-    //SRVO 3
-    for (pos3 = 0; pos3 <= 180; pos3 += 1){ //Moves 1 degree every time the loop cycles
-      SRVO3.write(pos3); //Pos3 changes within the for loop, and this command tells the servo to move to that position
-      delay(10); //A 10 ms delay
-    }
-
-    //SRVO 4
-    for (pos4 = 0; pos4 <= 180; pos4 += 1){ //Moves 1 degree every time the loop cycles
-      SRVO4.write(pos4); //Pos4 changes within the for loop, and this command tells the servo to move to that position
-      delay(10); //A 10 ms delay
-    }
-
-
-
-    //The second moves it from 180 degrees to 0 degrees.
-
-    //SRVO 1
-    for (pos1 = 180; pos1 >= 0; pos1 -= 1){
-      SRVO1.write(pos1);
-      delay(10);
-    }
-
-    //SRVO 2
-    for (pos2 = 180; pos2 >= 0; pos2 -= 1){
-      SRVO2.write(pos2);
-      delay(10);
-    }
-
-    //SRVO 3
-    for (pos3 = 180; pos3 >= 0; pos3 -= 1){
-      SRVO3.write(pos2);
-      delay(10);
-    }
-
-    //SRVO 4
-    for (pos4 = 180; pos4 >= 0; pos4 -= 1){
-      SRVO4.write(pos4);
-      delay(10);
-    }
+    RotateLeft (SRVO1, pos); //Makes SRVO1 rotate
+    RotateLeft (SRVO2, pos); //Makes SRVO2 rotate
+    RotateLeft (SRVO3, pos); //Makes SRVO3 rotate
+    RotateLeft (SRVO4, pos); //Makes SRVO4 rotate
 
     delay (1000);
 
