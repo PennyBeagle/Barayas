f942b0265064855b369d937c04358d15094f2482
Nick <sns56@students.uwf.edu>
2025-03-01 13:47:38 -0600

    wormed again

--- servospecific.cpp
+++ servospecific.cpp
@@ -9,6 +9,8 @@ Adafruit_PWMServoDriver srituhobby = Adafruit_PWMServoDriver();
 const int one = 24; //Defining one as pin 24 (LA)
 const int two = 25; //Defining two as pin 25 (LA)
 
+int worm = 0; //on & off button
+
 void setup() { //Sets up the driver library 
   Serial.begin(9600);
   srituhobby.begin();
@@ -16,9 +18,15 @@ void setup() { //Sets up the driver library
 
   pinMode(one, OUTPUT); //One is now an output (LA)
   pinMode(two, OUTPUT); //Two is now an output (LA)
+
+  pinMode(22, INPUT); //on & off button
 }
 
 void loop() {
+    worm = digitalRead(22); //on & off button
+    delay (4000); //4 second delay
+
+    while (worm = 1){
   //SERVO STUFF
   for (int servo = 0; servo < 3; servo++ ) { //Begins at servo 0, goes until servo 2
     for (int i = 0; i < servoMIN; i+= 50){
@@ -58,4 +66,5 @@ void loop() {
       digitalWrite(two, HIGH);
   
       delay(2000);
+}
 }
