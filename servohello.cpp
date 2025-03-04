ca1363d961f9f126fe193844ea99a22b0e2b13f7
Nick <sns56@students.uwf.edu>
2025-03-04 16:22:19 -0600

    edits

--- servohello.cpp
+++ servohello.cpp
@@ -25,14 +25,14 @@ void loop() {
     for (int servo = 2; servo >= 0; servo--) {  //Begins at servo 2, goes until servo 0
       for (int i = servoMIN; i < servoMAX; i += 3) {
         srituhobby.setPWM(servo, 0, i);  //Writes servo # to value servoMAX
-        delay(5);                       //0.2 second delay
+        delay(5);                       //5 ms delay
       }
     }
 
     for (int servo = 0; servo < 3; servo++) {  //Begins at servo 0, goes until servo 2
       for (int i = servoMAX; i > servoMIN; i -= 3) {
         srituhobby.setPWM(servo, 0, i);  //Writes servo # to value servoMIN
-        delay(5);                       //0.2 second delay
+        delay(5);                       //5 ms delay
       }
     }
 
